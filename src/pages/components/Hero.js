fc9a0360aa2103d957e6922a67249a635c156d11
Basir Khan <khanbasir5555@gmail.com>
2023-09-29 23:30:04 +0530

    stats added

--- src/pages/components/Hero.js
+++ src/pages/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Stats from "./Stats";
 const Hero = () => {
   return (
     <div>
@@ -7,13 +7,13 @@ const Hero = () => {
         <section className="pt-12 bg-none sm:pt-16">
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="max-w-2xl mx-auto text-center ">
-              {/* <h1 className="px-6 text-lg text-gray-200 font-inter">House of developers resources</h1> */}
+              <h1 className="px-6 text-lg text-gray-200 font-inter">Welcome Devlopers 🧑‍💻</h1>
               <p className="mt-5 text-4xl hero-text font-bold leading-tight text-white sm:leading-tight sm:text-5xl lg:text-6xl lg:leading-tight">
                 One Stop Solution For
                 {/* <span className="bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-lg filter opacity-30 w-full h-full absolute inset-0"></span> */}
                 <span className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text">
                   {" "}
-                  Developer Resources{" "}
+                  Developer{" "}
                 </span>
               </p>
 
@@ -24,10 +24,10 @@ const Hero = () => {
                   className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   role="button"
                 >
-                  Explore
+                  Explore More
                 </a>
 
-                <a
+                {/* <a
                   href="#"
                   title=""
                   className="inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold text-gray-200 transition-all duration-200 border-2 border-gray-400 sm:w-auto sm:mt-0 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 focus:bg-gray-900 hover:text-white focus:text-white hover:border-gray-900 focus:border-gray-900"
@@ -49,7 +49,7 @@ const Hero = () => {
                     />
                   </svg>
                   Watch free demo
-                </a>
+                </a> */}
               </div>
              
             </div>
@@ -58,7 +58,9 @@ const Hero = () => {
           <div className="pb-12 bg-none"></div>
         </section>
       </div>
+      <Stats/>
     </div>
+  
   );
 };
 
---
+++ src/pages/components/Stats.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Stats = () => {
+  return (
+    <div>
+      <div className=" flex flex-col md:flex-row bg-none gap-2">
+  <div className="flex flex-row m-auto bg-gradient-to-r from-purple-700 via-purple-800 to-purple-900 p-6 gap-8 rounded-lg border-2 border-purple-500">
+    <div className="my-auto">
+      <div className="text-lg text-purple-300">Free Tools</div>
+      <div className="text-4xl text-purple-100">622k</div>
+    </div>
+    <div className="text-purple-300 my-auto bg-gradient-to-l from-purple-700 via-purple-800 to-purple-900 rounded-full p-4">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M5.5 16a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 16h-8z" />
+      </svg>
+    </div>
+  </div>
+  <div className="flex flex-row m-auto bg-gradient-to-r from-purple-700 via-purple-800 to-purple-900 p-6 gap-8 rounded-lg border-2 border-purple-500">
+    <div className="my-auto">
+      <div className="text-lg text-purple-300">Downloads</div>
+      <div className="text-4xl text-purple-100">622k</div>
+    </div>
+    <div className="text-purple-300 my-auto bg-gradient-to-l from-purple-700 via-purple-800 to-purple-900 rounded-full p-4">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M5.5 16a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 16h-8z" />
+      </svg>
+    </div>
+  </div>
+  <div className="flex flex-row m-auto bg-gradient-to-r from-purple-700 via-purple-800 to-purple-900 p-6 gap-8 rounded-lg border-2 border-purple-500">
+    <div className="my-auto">
+      <div className="text-lg text-purple-300">Total Users</div>
+      <div className="text-4xl text-purple-100">622k</div>
+    </div>
+    <div className="text-purple-300 my-auto bg-gradient-to-l from-purple-700 via-purple-800 to-purple-900 rounded-full p-4">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M5.5 16a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 16h-8z" />
+      </svg>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Stats
