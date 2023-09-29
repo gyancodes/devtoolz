cbb0e40973ce2648fdeb7fe643aa5fe9dc5cbe8a
Basir Khan <khanbasir5555@gmail.com>
2023-09-29 22:19:45 +0530

    navbar ui fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
 import "@/styles/globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import("preline");
+  }, []);
   return (
     <>
       <Navbar />
