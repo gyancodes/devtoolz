77b634462b5dfae9834a46677af39651fbaf9960
Basir Khan <khanbasir5555@gmail.com>
2023-09-30 03:13:35 +0530

    login/signup added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,55 @@
-import "@/styles/globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { useEffect } from "react";
+import '@/styles/globals.css'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LoadingBar from 'react-top-loading-bar';
+import { useRouter } from 'next/router'
+import { useEffect,useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
+// import { HeroSection } from './Components/layouts/HeroSection'
 export default function App({ Component, pageProps }) {
+  const [progress, setProgress] = useState(0)
+  const [key , setKey] = useState(0)
+  const [user,setUser]=useState(false);
+  const router=useRouter();
   useEffect(() => {
-    import("preline");
-  }, []);
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer/>
-    </>
-  );
+    import('preline')
+  }, [])
+  useEffect(()=>{
+    
+    router.events.on('routeChangeStart',()=>{
+      setProgress(40)
+    })
+    router.events.on('routeChangeComplete',()=>{
+      setProgress(100)
+    })
+    const myUser = JSON.parse(localStorage.getItem('e-reviveUser'));
+    if(myUser){
+    setUser(true)
+    setKey(Math.random());
+  }
+ 
+  },[router.query])
+  const logout=()=>{
+    localStorage.removeItem('e-reviveUser');
+    setKey(Math.random());
+    toast.success("Logged out successfully", {
+      position: "top-right",
+    });
+    setTimeout(()=>{
+      router.push("/");
+    },1000);
+    setUser(false);
+  }
+  return <> 
+  <LoadingBar
+  color='#ff0f7b'
+  waitingTime={400}
+  progress={progress}
+  onLoaderFinished={() => setProgress(0)}
+/>
+<Toaster/>
+  <Navbar key={key} logout={logout} user={user}/>
+  {/* <HeroSection/> */}
+  
+  <Component {...pageProps} /> <Footer/></>
 }
---
+++ src/pages/signup.js
@@ -0,0 +1,292 @@
+import { set } from 'mongoose';
+import React from 'react'
+import toast, { Toaster } from 'react-hot-toast'
+import { useState,useEffect } from 'react';
+import {BiSolidUser } from 'react-icons/bi';
+import {GiWaterRecycling } from 'react-icons/gi';
+import Spinner from './components/Spinner';
+import Link from 'next/link';
+const Signup = () => {
+    const [val1, setVal1] = useState(true);
+    const [val2, setVal2] = useState(false);
+    const[loading, setLoading]=useState(false);
+    const[name,setName]=useState('');
+    const[email,setEmail]=useState('');
+    const[password,setPassword]=useState('');
+    const[cpassword,setcPassword]=useState('');
+    const[phone,setPhone]=useState('');
+    const[address,setAddress]=useState('');
+    const[pincode,setPincode]=useState('');
+    const[city,setCity]=useState('');
+    const[gst,setGst]=useState('');
+    const[state,setState]=useState('');
+    const[recycleUnitName,setcycleUnitName]=useState('');
+    const handleChanges=(e)=>{
+        if(e.target.name=="name"){
+            setName(e.target.value);
+        }
+        else if(e.target.name=="email"){
+            setEmail(e.target.value);
+        }
+        else if(e.target.name=="password"){
+            setPassword(e.target.value);
+        }
+       else if(e.target.name=="cpassword"){
+        setcPassword(e.target.value);
+       }
+       else if(e.target.name=="phone"){
+        setPhone(e.target.value);
+       }
+       else if(e.target.name=="address"){
+        setAddress(e.target.value);
+       }
+       else if(e.target.name=="pincode"){
+        setPincode(e.target.value);
+       }
+       else if(e.target.name=="city"){
+        setCity(e.target.value);
+       }
+       else if(e.target.name=="gst"){
+        setGst(e.target.value);
+       }
+       else if(e.target.name=="state"){
+        setState(e.target.value);
+       }
+       else if(e.target.name=="recycleUnitName"){
+        setcycleUnitName(e.target.value);
+       }
+    }
+    const handleSubmit=async(e)=>{
+      setLoading(true);
+        const datau={name:name,email:email,password:password,phone:phone,address:address};
+        const datar={name:name,email:email,password:password,phone:phone,address:address,pincode:pincode,gst:gst,state:state,pincode,city,recycleUnitName};
+        if(val1){
+            if(password!=cpassword){
+                toast.error('Your Password and Confirm Password are not matched', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else if(phone.length!=10){
+                toast.error('Your Phone Number Is Invalid', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else if(email.length==0){
+                toast.error('Please Enter Your Email', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else{
+                const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/usignup`, {
+                    method: "POST", // or 'PUT'
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(datau),
+                  });
+                  const response=await res.json();
+                  console.log(response);
+                  if(response.success){
+                    toast.success('Your account has been created successfully.', {
+                        position: "top-right",
+                      })
+                      setLoading(false);
+                  }
+                  else if(!response.success){
+                    toast.error(`${response.message}` ,{
+                        position: "top-right",
+                      })
+                      setLoading(false);
+                  }
+            }
+            
+        }
+        else if(val2){
+         if(password!=cpassword){ 
+            toast.error('Your Password and Confirm Password are not matched', {
+                position: "top-center",
+              })
+              setLoading(false);
+          }
+            else if(phone.length!=10){
+            toast.error('Your Phone Number Is Invalid', {
+                position: "top-center",
+              })
+              setLoading(false);
+            }
+            else if(email.length==0){
+                toast.error('Please Enter Your Email', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else if(recycleUnitName.length<=2){
+                toast.error('Please Enter Your RecyclingUnit Name above two characters', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else if(gst.length!=15){
+                toast.error('Please enter a valid gst number', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else if(address.length<=5){
+                toast.error('Please enter your address above 5 characters', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else if(pincode.length!=6){
+                toast.error('Please enter a valid pincode', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            // else if(city.length<=2){
+            //     toast.error('Please enter a valid city name', {
+            //         position: "top-center",
+            //       })
+            // }
+            else if(state.length<=2){
+                toast.error('Please enter a valid state name', {
+                    position: "top-center",
+                  })
+                  setLoading(false);
+            }
+            else{
+                const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/rsignup`, {
+                    method: "POST", // or 'PUT'
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(datar),
+                  });
+                  const response=await res.json();
+                  console.log(response)
+                  if(response.success){
+                    toast.success('Your account has been created successfully.', {
+                        position: "top-right",
+                      })
+                      setLoading(false);
+                  }
+                  else if(!response.success){
+                    toast.error(`${response.message}` ,{
+                        position: "top-right",
+                      })
+                      setLoading(false);
+                  }
+            }
+          
+        }
+    }
+  return (
+    <div>
+        <Toaster/>
+    { loading?<Spinner/>:<section className="bg-none">
+    <div className="flex justify-center">
+        
+
+        <div className="flex items-center w-full max-w-3xl p-4 mx-auto lg:px-4 lg:w-3/5">
+            <div className="w-full">
+                <h1 className="text-2xl font-semibold tracking-wider text-white capitalize dark:text-white">
+                    Create Your Account
+                </h1>
+
+                <p className="mt-4 text-gray-200 dark:text-gray-200">
+                    Let’s get you all set up so you can verify your personal account and begin setting up your profile.
+                </p>
+
+                <div className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200"> Name</label>
+                        <input type="text" placeholder="John" name= "name" value={name} onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">Phone number</label>
+                        <input type="number" placeholder="XXX-XX-XXXX-XXX" value={phone} name='phone' onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">Email address</label>
+                        <input type="email" placeholder="johnsnow@example.com" name="email" onChange={handleChanges} value={email} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+                    
+                    {val2?<>
+                        <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Recycling Unit Name</label>
+                        <input type="text" placeholder="Example Recycling Unit" value={recycleUnitName} name='recycleUnitName' onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">GSTIN</label>
+                        <input type="text" placeholder="2230KKBKI890" name='gst' value={gst} onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">Address</label>
+                        <input type="text" placeholder="ex:-24 sector New Delhi ,lane Road" name='address' value={address} onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">State</label>
+                        <input type="text" placeholder="ex:-Odisha" name='state' value={state} onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">Pin / Zip Code</label>
+                        <input type="number" placeholder="ex:-754293" name="pincode" value={pincode} onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+                    </>:""
+                    }
+                    <div>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">Password</label>
+                        <input type="password" placeholder="Enter your password" name="password" value={password} onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <div className='block'>
+                        <label className="block mb-2 text-sm text-gray-200 dark:text-gray-200">Confirm password</label>
+                        <input type="password" placeholder="Enter your password" name='cpassword' value={cpassword} onChange={handleChanges} className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+                  
+                   
+
+
+                    <button
+                        className="flex items-center justify-center w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-purple-600 rounded-lg hover:bg-purple-800 focus:outline-none focus:ring focus:ring-green-300 focus:ring-opacity-50 h-14" onClick={handleSubmit}>
+                        <span>Sign Up </span>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 rtl:-scale-x-100" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                    <div className=''>
+                    <a href="#" className="flex items-center justify-center p-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+    <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
+        <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
+        <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
+        <path d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z" fill="#4CAF50" />
+        <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
+    </svg>
+
+    <span className="mx-2 text-gray-200">Sign in with Google</span>
+</a>
+                    </div>
+                      <p className="mt-6 text-sm text-center text-gray-400 flex items-center mx-6 justify-center">Already have an account ? <Link href={"/login?user=true"} className="text-green-500 focus:outline-none focus:underline hover:underline">Sign in</Link>.</p>
+        
+                </div>
+            </div>
+        </div>
+    </div>
+</section>}
+    </div>
+  )
+}
+
+export default Signup
