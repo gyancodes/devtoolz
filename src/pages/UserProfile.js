fa28152471911769394814e24bb81b8f2704e373
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-09-30 21:59:36 +0530

    added team section

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const UserProfile = () => {
   return (
     <div>
-        
+      <section className="mx-auto flex max-w-screen-xl flex-col my-20 px-10 xl:px-20">
+        <div className="flex flex-col flex-shrink ">
+          <div className="flex justify-between">
+            
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
